6a48239f219c21f3a8e25b04cec87ae3b0e273ed
李俊峰 <lijunfeng@u51.com>
2018-04-03 11:49:03 +0800

    init

---
+++ Example/Pods/Target Support Files/JFKit/JFKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JFKit : NSObject
+@end
+@implementation PodsDummy_JFKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-JFKit_Tests/Pods-JFKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JFKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JFKit_Tests
+@end
